122130581c95e5daacb8506591c9ceb310837b4b
jinc132 <jinc132@u.washington.edu>
2019-01-30 12:05:36 -0800

    Add bellevue

--- Lab2.R
+++ Lab2.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(tigris)
 library(sf)
+library(raster)
+library(rgeos)
 
 options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
@@ -9,11 +11,12 @@ options(tigris_use_cache = TRUE)
 od <- read.csv('wa_od_main_JT00_2015.csv')
 rac <- read.csv('wa_rac_S000_JT00_2015.csv')
 wac <- read.csv('wa_wac_S000_JT00_2015.csv')
+shape <- st_read("CITYBDY.shp")
+shape <- shape %>%
+  filter(NAME = "BELLEVUE")
 
-wa_tract <- tracts(state = '53', county = '033')
-cb <- core_based_statistical_areas(cb = TRUE)
-
-bell <- block_groups("Washington", "Bellevue")
+wa_tract <- tracts(state = '53', county = '061')
+shape <- shape[shape$NAME == "BELLEVUE",]
 
 # Creates a copy of original 
 copy <- od
