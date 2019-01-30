4f6205dd68f53de8423c38820e423ba2af530e03
jinc132 <jinc132@u.washington.edu>
2019-01-30 11:34:54 -0800

    Re-add king county

--- Lab2.R
+++ Lab2.R
@@ -1,12 +1,20 @@
 library(tidyverse)
-#install.packages('tigris')
-install.packages('sf')
 library(tigris)
+library(sf)
 
 options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
 
 # Part 1
 od <- read.csv('wa_od_main_JT00_2015.csv')
 rac <- read.csv('wa_rac_S000_JT00_2015.csv')
 wac <- read.csv('wa_wac_S000_JT00_2015.csv')
+
+wa_tract <- tracts(state = '53', county = '033')
+cb <- core_based_statistical_areas(cb = TRUE)
+
+bell <- block_groups("Washington", "Bellevue")
+
+# Creates a copy of original 
+copy <- od
 
