24ee4b31121c50b19b25a830cdf6fcd7cc7834d5
jinc132 <jinc132@u.washington.edu>
2019-02-02 23:41:16 -0800

    relationship df

--- Lab2.R
+++ Lab2.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(tigris)
 library(sf)
 library(reshape2)
 
@@ -23,12 +22,21 @@ wa_tracts <- st_read("cb_2017_53_tract_500k.shp")
 bellevue <- wa_tracts %>% 
   filter(NAME %in% tnums)
 
-
 # Part 2
+
+# Creates and returns a dataframe of the relationship between hometract and
+# work tract with the specified key.
+CreateDf <- function(key) {
+  df <- dcast(bell_relate, work_tract ~ home_tract, value.var = key)
+  df[is.na(df)] <- 0
+  return (df)
+}
+
 ## OD data
 # setting and cleaning the table 
 od$w_geocode <- as.character(od$w_geocode)
 od$h_geocode <- as.character(od$h_geocode)
+
 od <- od %>%
   mutate(w_geocode = replace(w_geocode, length(w_geocode) == 14, "0"),
          h_geocode = replace(h_geocode, length(w_geocode) == 14, "0"))
@@ -44,8 +52,12 @@ colnames(bellevue)[5] <- paste("work_tract")
 overall_bell <- merge(bellevue, od, by ="work_tract", all.x = TRUE)
 
 # Creating block level and check for duplicates
-block<- unique(od[,1:2])
-nrow(block) == nrow(od)
+bell_block <- overall_bell %>%
+  select(w_geocode, h_geocode, S000, work_tract, home_tract)
+st_geometry(bell_block) <- NULL
+
+block<- unique(bell_block[,1:2])
+nrow(block) == nrow(bell_block)
 
 ## Added a count column for the number of sending blocks
 block$block_ct <- 1
@@ -54,29 +66,45 @@ block$block_ct <- 1
 work_block <- aggregate(. ~ w_geocode, data=block[,-2], FUN=sum)
 
 ## Use sum job variable S000 to get count of "jobs" to work block
-jobs_wb <- aggregate(. ~ w_geocode, data=od[,c(1:3)], FUN=sum)
+jobs_wb <- aggregate(. ~ w_geocode, data=bell_block[,c(1,3)], FUN=sum)
 
 ## Merge the two dataframes together by work block FIPS
 work_block <- merge(work_block, jobs_wb, by="w_geocode")
 
-## Creates a file with home_tract and w_geocode
-home_block <- unique(od[,c(1,15)])
+## Creates a data frame with home_tract and w_geocode
+block_w <- unique(bell_block[,c(1,5)])
 
 ## Added a count column for the number of sending tracts
-home_block$tract_ct <- 1
+block_w$tract_ct <- 1
 
-## Number of tracts "sending" jobs to work block
-home_block <- aggregate(. ~ w_geocode, data=home_block[,c(1,3)], FUN=sum)
-final_block <- merge(work_block, home_block, by="w_geocode")
+## Number of tracts "sending" jobs to work block and home block
+block_w <- aggregate(. ~ w_geocode, data=block_w[,c(1,3)], FUN=sum)
+final_wblock <- merge(work_block, block_w, by="w_geocode")
 
-# Produce a matrix with a worktract as rows and hometract as columns
+# Produce a dataframe with a worktract as rows and hometract as columns
 bell_relate <- overall_bell %>%
-  select(work_tract, home_tract, S000, SA01, SA02, SE01, SE02, SE03, SI01, SI02, SI03)
+  select(work_tract, home_tract, S000, SA01, SA02, SA03, SE01, SE02, SE03, SI01, SI02, SI03)
 
-total_tract <- dcast(bell_relate, work_tract ~ home_tract, value.var = "S000")
-total_tract[is.na(total_tract)] <- 0
-
-write.table(total_tract,"total_tract.csv")
+## Total number of jobs dataframe
+tract_tot <- CreateDf("S000")
+## Number of jobs of works age 29 or younger 
+tract_y <- CreateDf("SA01")
+## Number of jobs for worker age 30 to 54
+tract_m <- CreateDf("SA02")
+## Number of jobs for workers age 55 or older
+tract_o <- CreateDf("SA03")
+## Number of jobs for with earnings $1250/month or less
+tract_lin <- CreateDf("SE01")
+## Number of jobs with earnings $1251/month to $3333/month
+tract_min <- CreateDf("SE02")
+## Number of jobs with earnings greater than $3333/month
+tract_hin <- CreateDf("SE03")
+## Number of jobs in goods producing industry sector
+tract_gds <- CreateDf("SI01")
+## Number of jobs in trade, transportation, and utilties industry sector
+tract_tu <- CreateDf("SI02")
+## Number of jobs in all other services industry sectors
+tract_all <- CreateDf("SI03")
 
 # WAC
 wac$w_geocode <- as.character(wac$w_geocode)
